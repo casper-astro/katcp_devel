7917c4bb9839531e63a9d94f1c3e071dba18cf4e
Adam Barta <adam@ska.ac.za>
2011-01-24 10:15:56 +0200

    start of statemachine

---
+++ kcs/statemachine.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sysexits.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <katcp.h>
+#include <katcl.h>
+
+#include "kcs.h"
+
+
+int func1(int rtn){
+#ifdef DEBUG
+  fprintf(stderr,"Called Function 1 going to return: %d\n",rtn);
+#endif
+  return rtn;
+}
+
+int func2(int rtn){
+#ifdef DEBUG
+  fprintf(stderr,"Called Function 2 going to return: %d\n",rtn);
+#endif
+  return rtn;
+}
+
+int func3(int rtn){
+#ifdef DEBUG
+  fprintf(stderr,"Called Function 3 going to return: %d\n",rtn);
+#endif
+  return rtn;
+}
+
+
+#ifdef STANDALONE
+int main(int argc, char **argv){
+  
+  int i;
+  int cf;
+  int (*statemachine[])(int) = {
+    &func1,
+    &func2,
+    &func3,
+    NULL
+  };
+
+  for (i=0; statemachine[i]; i++)
+  {
+    
+    cf = (*statemachine[i])(i);
+    
+#ifdef DEBUG
+      fprintf(stderr,"function returned: %d\n",cf);
+#endif
+  
+  }
+
+  return EX_OK;
+}
+#endif
