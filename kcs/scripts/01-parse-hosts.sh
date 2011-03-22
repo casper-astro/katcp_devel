ccecd175fd26b7adff07173909f739e7aae39926
Adam Barta <adam@kat7cntrl.kat.ac.za>
2011-03-22 19:05:58 +0200

    add testing getxports bash script

--- kcs/scripts/01-parse-hosts.sh
+++ kcs/scripts/01-parse-hosts.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-workingdir=~/work/katcp-git
+#workingdir=~/work/katcp-git
+workingdir=~/katcp-git
 
-hostsfile=$workingdir/kcs/scripts/hosts
-kcpcmd="$workingdir/cmd/kcpcmd -q -s localhost:7147 roach add"
+#hostsfile=$workingdir/kcs/scripts/hosts
+hostfile=/etc/hosts
+#kcpcmd="$workingdir/cmd/kcpcmd -q -s localhost:7147 roach add"
+kcpcmd=echo
 
-grep roach.*x $hostsfile | tr -s \  | (while read myip myhost; do $kcpcmd $(echo katcp://$myhost:7147/ | cut -f1 -d.) $myip xport ; done)
+grep roach.*x $hostsfile | tr -s \  | (while read myip myhost; do $kcpcmd $(echo xport://$myhost:10001/ | cut -f1 -d.) $myip xport ; done)
 
-grep 'roach[[:digit:]]*\.' $hostsfile | tr -s \ | (while read myip myhost; do $kcpcmd $(echo katcp://$myhost:7147/ | cut -f1 -d.) $myip spare ; done)
+#grep 'roach[[:digit:]]*\.' $hostsfile | tr -s \ | (while read myip myhost; do $kcpcmd $(echo katcp://$myhost:7147/ | cut -f1 -d.) $myip spare ; done)
