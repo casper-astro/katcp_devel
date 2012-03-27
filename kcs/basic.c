68b56ec82dedb61210b789a4aa16c626b8c0aee8
Marc Welz <marc@ska.ac.za>
2012-03-27 10:58:55 +0200

    Make malformed commands a lesser failure

--- kcs/basic.c
+++ kcs/basic.c
@@ -64,7 +64,7 @@ int script_wildcard_cmd(struct katcp_dispatch *d, int argc)
 
   name = create_kurl_from_string_katcp(arg_string_katcp(d, 0)+1);
   if(name == NULL){
-    log_message_katcp(d, KATCP_LEVEL_FATAL, NULL, "logic problem, unable to acquire command name possibly try uri exec:///path/to/process");
+    log_message_katcp(d, KATCP_LEVEL_ERROR, NULL, "logic problem, unable to acquire command name possibly try uri exec:///path/to/process");
     destroy_kurl_katcp(name);
     return KATCP_RESULT_FAIL;
   }
