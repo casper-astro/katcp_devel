7d8124ba58006b5130346eaf784717b7c5035308
Marc Welz <marc@ska.ac.za>
2012-08-07 17:54:17 +0200

    Custom delay invocation, to time things

--- delay/delay.c
+++ delay/delay.c
@@ -19,16 +19,35 @@
 
 #define NAME   "k7-delay"
 
+int complete_with_log(struct katcl_line *l, struct timeval *until, struct katcl_line *k)
+{
+  struct katcl_parse *px;
+  int result;
+  char *ptr;
+
+  while((result = complete_rpc_katcl(l, 0, until)) == 0){
+    px = ready_katcl(l);
+    if(px){
+      if(is_inform_parse_katcl(px)){
+        ptr = get_string_parse_katcl(px, 0);
+        if(!strcmp("#log", ptr)){
+          append_parse_katcl(k, px);
+        }
+      }
+    }
+  }
+
+  return result;
+}
+
 int main(int argc, char **argv)
 {
   struct katcl_line *l, *k;
   struct timeval start, ready, done, until, delta, request, elapsed, lead, local;
-  int result, total, i, code;
+  int result, code;
   char *server, *end, *ptr;
   long double value, tmp;
-  char buffer[BUFFER];
 
-  code = 1; /* assume a failure */
   gettimeofday(&start, NULL);
 
   /* preparation */
@@ -40,7 +59,7 @@ int main(int argc, char **argv)
   }
 
   if(argc <= 6){
-    fprintf(stderr, "usage: %s antpol loadtime delay delayrate fringe fringerate\n", argv[0]);
+    sync_message_katcl(k, KATCP_LEVEL_ERROR, NAME, "expect parameters: antpol loadtime delay delayrate fringe fringerate");
     return 1;
   }
 
@@ -68,8 +87,8 @@ int main(int argc, char **argv)
   value = strtold(argv[4], &end); /* delay rate */
   append_args_katcl(l, KATCP_FLAG_STRING , "%.8Lf", value);
 
-  value = strtold(argv[2], &end); /* time */
-  append_args_katcl(l, KATCP_FLAG_STRING , "%Lf", value / 1000.0);
+  value = strtold(argv[2], &end) / 1000.0; /* time */
+  append_args_katcl(l, KATCP_FLAG_STRING , "%Lf", value);
 
   tmp = truncl(value);
   request.tv_sec = tmp;
@@ -77,42 +96,46 @@ int main(int argc, char **argv)
 
   append_string_katcl(l, KATCP_FLAG_STRING | KATCP_FLAG_LAST, "now");
 
-
   gettimeofday(&ready, NULL);
 
   if(cmp_time_katcp(&request, &ready) <= 0){
     sub_time_katcp(&delta, &ready, &request);
 
     sync_message_katcl(k, KATCP_LEVEL_ERROR, NAME, "requested time %lu.%06lus is %lu.%06lus in the past", request.tv_sec, request.tv_usec, delta.tv_sec, delta.tv_usec);
-    return 1;
+    code = 1;
+
+  } else {
+
+    /* the send part */
+
+    delta.tv_sec = 20;
+    delta.tv_usec = 0;
+
+    add_time_katcp(&until, &ready, &delta);
+
+    result = complete_with_log(l, &until, k);
+
+    if(result < 0){
+      sync_message_katcl(k, KATCP_LEVEL_ERROR, NAME, "unable to complete request");
+      code = 2;
+    } else {
+      ptr = arg_string_katcl(l, 1);
+      if(ptr){
+        if(strcmp(ptr, KATCP_OK)){
+          append_string_katcl(l, KATCP_FLAG_STRING | KATCP_FLAG_FIRST | KATCP_FLAG_LAST, "?get-log");
+          complete_with_log(l, &until, k);
+
+          append_string_katcl(l, KATCP_FLAG_STRING | KATCP_FLAG_FIRST | KATCP_FLAG_LAST, "?clr-log");
+          complete_with_log(l, &until, k);
+          code = 1;
+        } else {
+          code = 0;
+        }
+      } else {
+        code = 2;
+      }
+    }
   }
-
-  /* the send part */
-
-  delta.tv_sec = 20;
-  delta.tv_usec = 0;
-
-  add_time_katcp(&until, &ready, &delta);
-
-  while((result = complete_rpc_katcl(l, 0, &until)) == 0){
-#ifdef DEBUG
-    fprintf(stderr, "got message\n");
-#endif
-  }
-
-  if(result < 0){
-    /* TODO: end connection, request/replies potentially out of sync */
-    return 3;
-  }
-
-  ptr = arg_string_katcl(l, 1);
-  if(ptr == NULL){
-    return -1;
-  }
-
-  if(strcmp(ptr, KATCP_OK)){
-    code = 0;
-  } 
 
   gettimeofday(&done, NULL);
 
@@ -122,7 +145,7 @@ int main(int argc, char **argv)
   sub_time_katcp(&elapsed, &done, &start);
   sub_time_katcp(&local, &ready, &start);
 
-  sync_message_katcl(k, KATCP_LEVEL_DEBUG, NAME, "delay stats: lead=%lu.%06lus taken=%lu.%06lus setup=%lu.%06lus", lead.tv_sec, lead.tv_usec, elapsed.tv_sec, elapsed.tv_usec, local.tv_sec, local.tv_usec);
+  sync_message_katcl(k, KATCP_LEVEL_DEBUG, NAME, "delay stats: lead=%lu.%06lus taken=%lu.%06lus setup=%lu.%06lus code=%d", lead.tv_sec, lead.tv_usec, elapsed.tv_sec, elapsed.tv_usec, local.tv_sec, local.tv_usec, code);
 
   destroy_rpc_katcl(k);
 
