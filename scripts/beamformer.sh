691f88f12269193a78fe8aa7e67c49e597177c71
Marc Welz <marc@ska.ac.za>
2012-10-30 17:48:21 +0200

    More untested beamformer logic

---
+++ scripts/beamformer.sh
@@ -0,0 +1,100 @@
+kcs_export() {
+  if [ $# -lt 1 ] ; then
+    exit 1
+  fi
+
+  kcpcmd -s localhost:7147 -k -i -r setenv $1 $2
+}
+
+ip_to_hex() {
+  if [ $# -lt 1 ] ; then
+    return 1
+  fi
+
+  ip=$1
+
+  hexip=$(echo $ip | tr '.' \\n | (echo obase=16 ; r=0 ; while read line ; do r=$[256*$r+$line] ; done ; echo $r ) | bc )
+
+  echo "ip $ip is $hexip"
+
+  export hexip
+}
+
+port_to_hex() {
+  if [ $# -lt 1 ] ; then
+    return 1
+  fi
+
+  port=$1
+
+  hexport=$(echo -e "obase=16\n${port}" | bc )
+
+  echo "port $port is $hexport"
+
+  export hexport
+}
+
+detect_reg_set () {
+  if [ $# -lt 2 ] ; then
+    return 1
+  fi
+
+  reg=$1
+  shift
+
+  kcppar -s $* -x wordread $reg 0 
+}
+
+detect_bf_count () {
+  if [ $# -lt 1 ] ; then
+    kcs_export KCS_BF_COUNT 0
+    return 1
+  fi
+
+  i=0
+
+  while detect_reg_set bf${i}_gbe_out_port0 $*; do
+    if [ $i -gt 8 ] ; then
+      kcpmsg -l error "unreasonably large number of beamformers $i"
+      kcs_export KCS_BF_COUNT 0
+      return 1;
+    fi
+    kcs_export KCS_BF${i}_RX 0.0.0.0:0
+    i=$[i+1]
+  done
+
+  if [ $? -ne 1 ] ; then
+    kcs_export KCS_BF_COUNT 0
+    return 1
+  fi
+
+  kcs_export KCS_BF_COUNT $i
+}
+
+set_bf_rx () {
+  if [ $# -lt 4 ] ; then
+    return 1
+  fi
+
+  if [ "${1:0:2}" != "bf" ] ; then
+    return 1
+  fi
+
+  beam=${1:2}
+
+  if [ $beam -ge ${KCS_BF_COUNT} ] ; then
+    return 1
+  fi
+
+  ip=$2
+  port=$3
+
+  ip_to_hex $ip
+  port_to_hex $port
+
+  shift 3
+
+  if kcppar -s $* -x wordwrite bf${beam}_gbe_out_port0 0 0x${hexport} -x wordwrite bf${beam}_gbe_out_ip0 0 0x${hexip} ; then
+    kcs_export KCS_BF${beam}_RX ${ip}:${port}
+  fi
+}
