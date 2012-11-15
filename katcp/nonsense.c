77f1e253c5a26d2c850dcef4427b6910cb7d000e
Marc Welz <marc@ska.ac.za>
2012-11-15 17:12:11 +0200

    Bugfix: Inverted version preprocessor test

--- katcp/nonsense.c
+++ katcp/nonsense.c
@@ -3463,7 +3463,9 @@ static int reply_sensor_sampling_katcp(struct katcp_dispatch *d, struct katcp_se
 {
   char *strategy;
   int result, extra;
+#if KATCP_PROTOCOL_MAJOR_VERSION < 5 
   unsigned long period;
+#endif
   struct katcp_nonsense *ns;
   extra = 0; /* paranoid tautology */
 
@@ -3514,7 +3516,7 @@ static int reply_sensor_sampling_katcp(struct katcp_dispatch *d, struct katcp_se
 #endif
     switch(ns->n_strategy){
       case KATCP_STRATEGY_PERIOD :
-#if KATCP_PROTOCOL_MAJOR_VERSION < 5 
+#if KATCP_PROTOCOL_MAJOR_VERSION >= 5 
         result = append_args_katcp(d, KATCP_FLAG_LAST | KATCP_FLAG_STRING, "%lu.%06lu", ns->n_period.tv_sec, ns->n_period.tv_usec);
 #else
         period = (ns->n_period.tv_sec * 1000) + (ns->n_period.tv_usec / 1000);
