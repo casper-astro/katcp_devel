58cbcf433d0cdbea80007461edc0e7577aeac92d
Marc Welz <marc@ska.ac.za>
2013-01-21 11:38:12 +0200

    Make job debugs slightly less important

--- katcp/job.c
+++ katcp/job.c
@@ -320,7 +320,7 @@ static int relay_cmd_job_katcp(struct katcp_dispatch *d, struct katcp_notice *n,
   log_message_katcp(d, KATCP_LEVEL_DEBUG, NULL, "about to relay cmd message %s ...", inform);
   
   if(!strcmp(inform, KATCP_RETURN_JOB)){
-    log_message_katcp(d, KATCP_LEVEL_DEBUG, NULL, "detaching command match in response to %s", inform);
+    log_message_katcp(d, KATCP_LEVEL_TRACE, NULL, "detaching command match in response to %s", inform);
     return 0;
   }
 
@@ -423,7 +423,7 @@ static int get_request_job_katcp(struct katcp_dispatch *d, struct katcp_notice *
   }
   
   if(!strcmp(cmd, KATCP_RETURN_JOB)){
-    log_message_katcp(d, KATCP_LEVEL_DEBUG, NULL, "detaching get command match in response to %s", cmd);
+    log_message_katcp(d, KATCP_LEVEL_TRACE, NULL, "detaching get command match in response to %s", cmd);
     return 0;
   }
 
@@ -468,7 +468,7 @@ static int set_request_job_katcp(struct katcp_dispatch *d, struct katcp_notice *
   }
   
   if(!strcmp(cmd, KATCP_RETURN_JOB)){
-    log_message_katcp(d, KATCP_LEVEL_DEBUG, NULL, "detaching set command match in response to %s", cmd);
+    log_message_katcp(d, KATCP_LEVEL_TRACE, NULL, "detaching set command match in response to %s", cmd);
     return 0;
   }
 
@@ -515,7 +515,7 @@ static int dict_request_job_katcp(struct katcp_dispatch *d, struct katcp_notice 
     return 0;
 
   if(!strcmp(cmd, KATCP_RETURN_JOB)){
-    log_message_katcp(d, KATCP_LEVEL_DEBUG, NULL, "detaching dict command match in response to %s", cmd);
+    log_message_katcp(d, KATCP_LEVEL_TRACE, NULL, "detaching dict command match in response to %s", cmd);
     return 0;
   }
 
@@ -551,7 +551,7 @@ static int relay_log_job_katcp(struct katcp_dispatch *d, struct katcp_notice *n,
   }
   
   if(!strcmp(inform, KATCP_RETURN_JOB)){
-    log_message_katcp(d, KATCP_LEVEL_DEBUG, NULL, "detaching log match in response to %s", inform);
+    log_message_katcp(d, KATCP_LEVEL_TRACE, NULL, "detaching log match in response to %s", inform);
     return 0;
   }
 
@@ -576,7 +576,7 @@ static int hold_version_job_katcp(struct katcp_dispatch *d, struct katcp_notice 
   }
   
   if(!strcmp(inform, KATCP_RETURN_JOB)){
-    log_message_katcp(d, KATCP_LEVEL_DEBUG, NULL, "detaching in response to %s", inform);
+    log_message_katcp(d, KATCP_LEVEL_TRACE NULL, "detaching in response to %s", inform);
     return 0;
   }
 
