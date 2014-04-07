1c493623bb7f804cdc54c71f4d8d4a530f6ca34c
Marc Welz <marc@ska.ac.za>
2014-04-07 14:44:07 +0200

    Allow parameter substitution for forwards

--- katcp/dpx-forward.c
+++ katcp/dpx-forward.c
@@ -23,38 +23,326 @@
 #define FORWARD_STATE_MAGIC 0xfa34df12
 #endif
 
+#define FOP_LITTERAL        0
+#define FOP_SINGLE          1
+#define FOP_TRAIL           2
+#define FOP_VAR             3
+#define FOP_SPECIAL_GROUP   4
+#define FOP_SPECIAL_CLIENT  5
+
+#define MAX_FOP             6
+
+struct forward_symbolic_parm
+{
+  unsigned short p_op;
+  unsigned short p_num;
+  char *p_str;
+};
+
 struct forward_symbolic_state
 {
 #ifdef KATCP_CONSISTENCY_CHECKS 
   unsigned int f_magic;
 #endif
   char *f_peer;
-  char *f_as;
+  struct forward_symbolic_parm *f_vector;
+  unsigned int f_count;
 };
+
+/************************************************************************/
+
+/************************************************************************/
+
+int load_parm_forward(struct katcp_dispatch *d, struct forward_symbolic_state *fs, unsigned int op, unsigned int number, char *string)
+{
+  struct forward_symbolic_parm *pf, *pv;
+  char *copy;
+
+  pv = realloc(fs->f_vector, (fs->f_count + 1) * (sizeof(struct forward_symbolic_parm)));
+  if(pv == NULL){
+    return -1;
+  }
+  fs->f_vector = pv;
+
+  if(string){
+    copy = strdup(string);
+    if(copy == NULL){
+      return -1;
+    }
+  } else {
+    copy = NULL;
+  }
+
+#ifdef DEBUG
+  fprintf(stderr, "forwarding: loading parameter %u: op=%d, number=%u, ptr=%p\n", fs->f_count, op, number, copy);
+#endif
+
+  pf = &(fs->f_vector[fs->f_count]);
+
+  pf->p_op = op;
+  pf->p_num = number;
+  pf->p_str = copy;
+
+  fs->f_count = fs->f_count + 1;
+
+  return 0;
+}
+
+/*************************/
+
+/* %0 %1 ... %N */
+/* %0+ %1+ ...  */
+/* %g %c - group and client */
+/* $variable */
+
+/*************************/
+
+int parse_parm_foward(struct katcp_dispatch *d, struct forward_symbolic_state *fs, char *ptr)
+{
+  unsigned int op, num;
+  char *scan, *end;
+
+  if(ptr == NULL){
+    return -1;
+  }
+
+  op = MAX_FOP;
+  num = 0;
+  scan = NULL;
+
+  if(ptr[0] == '%'){
+    num = strtoul(ptr + 1, &end, 10);
+    switch(end[0]){
+      case '+' :
+        op = FOP_TRAIL;
+        break;
+      case '\n' :
+      case '\r' :
+      case '\0' :
+        op = FOP_SINGLE;
+        break;
+      case 'g' :
+        if(end == (ptr + 1)){
+          op = FOP_SPECIAL_GROUP;
+          break;
+        }
+        /* WARNING: extra stupid FALL, should be a goto default */
+      case 'c' :
+        if(end == (ptr + 1)){
+          op = FOP_SPECIAL_CLIENT;
+          break;
+        }
+        /* CONDITIONAL FALL */
+      default :
+        return -1;
+        break;
+    }
+  } else if(ptr[0] == '$'){
+    scan = ptr + 1;
+    op = FOP_VAR;
+  } else {
+    op = FOP_LITTERAL;
+    scan = ptr;
+  }
+
+  return load_parm_forward(d, fs, op, num, scan);
+}
+
+struct katcl_parse *generate_relay_forward(struct katcp_dispatch *d, struct forward_symbolic_state *fs, char *name)
+{
+  struct katcl_parse *px, *po;
+  struct forward_symbolic_parm *pf;
+  struct katcp_vrbl *vx;
+  struct katcp_flat *fx;
+  struct katcp_group *gx;
+  unsigned int size, limit;
+  int flags, run, result, i;
+
+  px = create_parse_katcl();
+  if(px == NULL){
+    return NULL;
+  }
+
+  add_string_parse_katcl(px, KATCP_FLAG_FIRST | KATCP_FLAG_STRING, "?relay");
+  add_string_parse_katcl(px, KATCP_FLAG_STRING, name);
+
+  po = arg_parse_katcp(d);
+  if(po == NULL){
+    destroy_parse_katcl(px);
+    return NULL;
+  }
+  size = get_count_parse_katcl(po);
+
+  if(fs->f_count <= 0){
+    log_message_katcp(d, KATCP_LEVEL_FATAL, NULL, "internal error where a message triggering a relay is entirely empty");
+    destroy_parse_katcl(px);
+    return NULL;
+  }
+
+  limit = fs->f_count - 1;
+  for(run = 1; run > 0;){
+
+    pf = &(fs->f_vector[limit]);
+    switch(pf->p_op){
+      case FOP_SINGLE :
+      case FOP_TRAIL :
+        if(pf->p_num >= size){
+#ifdef DEBUG
+          fprintf(stderr, "forwarding: ignoring trailing parameter %u (want %u, currently have %u)\n", limit, pf->p_num, size);
+#endif
+          limit--;
+        } else {
+          run = 0;
+        }
+        break;
+      case FOP_VAR :
+        if(find_vrbl_katcp(d, pf->p_str) == NULL){
+          limit--;
+        } else {
+          run = 0;
+        }
+        break;
+      default :
+        run = 0;
+        break;
+    }
+
+    if(limit == 0){
+      log_message_katcp(d, KATCP_LEVEL_ERROR, NULL, "malformed relay where nothing can be forwarded");
+      destroy_parse_katcl(px);
+      return NULL;
+    }
+  }
+
+#ifdef DEBUG
+  fprintf(stderr, "forwarding: intend to process %u positions\n", limit);
+#endif
+
+  for(i = 0; i <= limit; i++){
+    pf = &(fs->f_vector[i]);
+    flags = (i == limit) ? KATCP_FLAG_LAST : 0;
+    result = 0;
+
+    switch(pf->p_op){
+      case FOP_LITTERAL :
+        result = add_string_parse_katcl(px, KATCP_FLAG_STRING | flags, pf->p_str);
+        break;
+      case FOP_SINGLE :
+      case FOP_TRAIL :
+        if(pf->p_num < size){
+          if(pf->p_op == FOP_SINGLE){
+            result = add_parameter_parse_katcl(px, flags, po, pf->p_num);
+          } else {
+            result = add_trailing_parse_katcl(px, flags, po, pf->p_num);
+          }
+        } else {
+          result = (-1);
+        }
+        break;
+      case FOP_VAR :
+        vx = find_vrbl_katcp(d, pf->p_str);
+        if(vx){
+          result = add_payload_vrbl_katcp(d, px, flags, vx, NULL);
+        } else {
+          result = (-1);
+        }
+        break;
+      case FOP_SPECIAL_GROUP :
+        gx = this_group_katcp(d);
+        if(gx && gx->g_name){
+          result = add_string_parse_katcl(px, flags, gx->g_name);
+        } else {
+          result = (-1);
+        }
+        break;
+      case FOP_SPECIAL_CLIENT :
+        fx = this_flat_katcp(d);
+        if(fx && fx->f_name){
+          result = add_string_parse_katcl(px, flags, fx->f_name);
+        } else {
+          result = (-1);
+        }
+        break;
+      default :
+#ifdef KATCP_CONSISTENCY_CHECKS 
+        fprintf(stderr, "major logic problem: unknown op %u at %p while relaying request\n", pf->p_op, pf);
+        abort();
+#endif
+        result = (-1);
+    }
+
+    if(result < 0){
+      destroy_parse_katcl(px);
+      return NULL;
+    }
+  }
+
+  return px;
+}
+
+/************************************************************************/
+
+struct forward_symbolic_state *create_forward_symbolic_state(struct katcp_dispatch *d)
+{
+  struct forward_symbolic_state *fs;
+ 
+  fs = malloc(sizeof(struct forward_symbolic_state));
+  if(fs == NULL){
+    log_message_katcp(d, KATCP_LEVEL_ERROR, NULL, "unable to allocate symbolic state");
+    return NULL;
+  }
+
+#ifdef KATCP_CONSISTENCY_CHECKS 
+  fs->f_magic = FORWARD_STATE_MAGIC;
+#endif
+
+  fs->f_peer = NULL;
+
+  fs->f_vector = NULL;
+  fs->f_count = 0;
+
+  return fs;
+}
 
 void clear_forward_symbolic_state(void *data)
 {
   struct forward_symbolic_state *fs;
+  struct forward_symbolic_parm *pf;
+  unsigned int i;
 
   fs = data;
-  if(fs){
-#ifdef KATCP_CONSISTENCY_CHECKS 
-    if(fs->f_magic != FORWARD_STATE_MAGIC){
-      fprintf(stderr, "logic problem: expected %p to be a symbolic state structure, but magic 0x%x\n", fs, fs->f_magic);
-      abort();
-    }
-    fs->f_magic = 0;
-#endif
-    if(fs->f_peer){
-      free(fs->f_peer);
-      fs->f_peer = NULL;
-    }
-    if(fs->f_as){
-      free(fs->f_as);
-      fs->f_as = NULL;
-    }
-    free(fs);
+  if(fs == NULL){
+    return;
   }
+
+#ifdef KATCP_CONSISTENCY_CHECKS 
+  if(fs->f_magic != FORWARD_STATE_MAGIC){
+    fprintf(stderr, "logic problem: expected %p to be a symbolic state structure, but magic 0x%x\n", fs, fs->f_magic);
+    abort();
+  }
+  fs->f_magic = 0;
+#endif
+
+  if(fs->f_peer){
+    free(fs->f_peer);
+    fs->f_peer = NULL;
+  }
+
+  if(fs->f_vector){
+    for(i = 0; i < fs->f_count; i++){
+      pf = &(fs->f_vector[i]);
+      if(pf->p_str){
+        free(pf->p_str);
+        pf->p_str = NULL;
+      }
+    }
+    free(fs->f_vector);
+    fs->f_vector = NULL;
+  }
+
+  fs->f_count = 0;
+
+  free(fs);
 }
 
 int complete_relay_generic_group_cmd_katcp(struct katcp_dispatch *d, int argc)
@@ -111,9 +399,12 @@ int complete_relay_generic_group_cmd_katcp(struct katcp_dispatch *d, int argc)
     prepend_inform_katcp(d);
   }
 
-  /* TODO - what about a px without any parameters ? */
-
-  append_trailing_katcp(d, KATCP_FLAG_LAST, px, 1);
+  if(get_count_parse_katcl(px) > 1){
+    append_trailing_katcp(d, KATCP_FLAG_LAST, px, 1);
+  } else {
+    /* treat px without any parameters as a failure */
+    append_string_katcp(d, KATCP_FLAG_STRING | KATCP_FLAG_LAST, KATCP_FAIL);
+  }
 
 #if 0
   /* TODO relay the response payload */
@@ -231,7 +522,7 @@ int perform_forward_symbolic_group_cmd_katcp(struct katcp_dispatch *d, int argc)
   struct katcp_flat *fx, *fy;
   struct forward_symbolic_state *fs;
   struct katcp_endpoint *target, *source, *origin;
-  struct katcl_parse *px, *po;
+  struct katcl_parse *px;
   char *req;
 
   req = arg_string_katcp(d, 0);
@@ -284,25 +575,9 @@ int perform_forward_symbolic_group_cmd_katcp(struct katcp_dispatch *d, int argc)
     target = handler_of_flat_katcp(d, fy);
   }
 
-  px = create_parse_katcl();
+  px = generate_relay_forward(d, fs, fy->f_name);
   if(px == NULL){
     return extra_response_katcp(d, KATCP_RESULT_FAIL, KATCP_FAIL_MALLOC);
-  }
-
-  add_string_parse_katcl(px, KATCP_FLAG_FIRST | KATCP_FLAG_STRING, "?relay");
-  add_string_parse_katcl(px, KATCP_FLAG_STRING, fy->f_name);
-
-  po = arg_parse_katcp(d);
-
-  if(fs->f_as){
-    if(argc <= 1){
-      add_string_parse_katcl(px, KATCP_FLAG_STRING | KATCP_FLAG_LAST, fs->f_as);
-    } else {
-      add_string_parse_katcl(px, KATCP_FLAG_STRING, fs->f_as);
-      add_trailing_parse_katcl(px, KATCP_FLAG_LAST, po, 1);
-    }
-  } else {
-    add_trailing_parse_katcl(px, KATCP_FLAG_LAST, po, 0);
   }
 
   if(send_message_endpoint_katcp(d, source, target, px, 1) < 0){
@@ -311,13 +586,12 @@ int perform_forward_symbolic_group_cmd_katcp(struct katcp_dispatch *d, int argc)
     return KATCP_RESULT_FAIL;
   }
 
-
 #if 0
   /* TODO: use wrappers to access fx members */
   /* this code path is just added, presumed nodefective, defer testing it for some other time */
   origin = sender_to_flat_katcp(d, fx);
 
-  if(callback_flat_katcp(d, origin, po, target, &complete_relay_generic_group_cmd_katcp, "relay", KATCP_REPLY_HANDLE_REPLIES | KATCP_REPLY_HANDLE_INFORMS)){
+  if(callback_flat_katcp(d, origin, po, target, &complete_relay_feneric_group_cmd_katcp, "relay", KATCP_REPLY_HANDLE_REPLIES | KATCP_REPLY_HANDLE_INFORMS)){
     log_message_katcp(d, KATCP_LEVEL_WARN, NULL, "unable to register relay callback for %s", req);
     return KATCP_RESULT_FAIL;
   }
@@ -334,10 +608,11 @@ int perform_forward_symbolic_group_cmd_katcp(struct katcp_dispatch *d, int argc)
 
 int forward_symbolic_group_cmd_katcp(struct katcp_dispatch *d, int argc)
 {
-  char *cmd, *ptr;
+  char *cmd, *ptr, *str;
   struct forward_symbolic_state *fs;
   struct katcp_cmd_map *mx;
   struct katcp_flat *fx;
+  unsigned int i;
 
   fx = this_flat_katcp(d);
   if(fx == NULL){
@@ -362,37 +637,47 @@ int forward_symbolic_group_cmd_katcp(struct katcp_dispatch *d, int argc)
     return extra_response_katcp(d, KATCP_RESULT_INVALID, KATCP_FAIL_USAGE);
   }
 
-  /* this isn't quite ideal, we could end up handling a request handler within an inform map. Maybe eliminate the ptr code completely, not needed by add_full_cmd_map anyway */
-  ptr = default_message_type_katcm(cmd, KATCP_REQUEST);
-  if(ptr == NULL){
-    log_message_katcp(d, KATCP_LEVEL_ERROR, NULL, "unable to duplicate %s", cmd);
-    return extra_response_katcp(d, KATCP_RESULT_FAIL, KATCP_FAIL_MALLOC);
-  }
-
-  fs = malloc(sizeof(struct forward_symbolic_state));
+  fs = create_forward_symbolic_state(d);
   if(fs == NULL){
-    free(ptr);
     log_message_katcp(d, KATCP_LEVEL_ERROR, NULL, "unable to allocate symbolic state");
     return extra_response_katcp(d, KATCP_RESULT_FAIL, KATCP_FAIL_MALLOC);
   }
 
-#ifdef KATCP_CONSISTENCY_CHECKS 
-  fs->f_magic = FORWARD_STATE_MAGIC;
-#endif
-
-  fs->f_peer = NULL;
-  fs->f_as = NULL;
-
   fs->f_peer = arg_copy_string_katcp(d, 2);
-  if(argc > 3){
-    fs->f_as = arg_copy_string_katcp(d, 3);
+  if(fs->f_peer == NULL){
+    log_message_katcp(d, KATCP_LEVEL_ERROR, NULL, "no valid destination could be found or copied");
+    clear_forward_symbolic_state(fs);
+    return extra_response_katcp(d, KATCP_RESULT_FAIL, KATCP_FAIL_USAGE);
   }
 
-  if((fs->f_peer == NULL) || ((argc > 3) && (fs->f_as == NULL))){
-    log_message_katcp(d, KATCP_LEVEL_ERROR, NULL, "unable to copy parameters, argument count is %d", argc);
-    free(ptr);
+  for(i = 3; i < argc; i++){
+    str = arg_string_katcp(d, i);
+    if(str == NULL){
+      log_message_katcp(d, KATCP_LEVEL_ERROR, NULL, "refusing to relay a null parameter at %u", i);
+      clear_forward_symbolic_state(fs);
+      return extra_response_katcp(d, KATCP_RESULT_FAIL, KATCP_FAIL_USAGE);
+    }
+    if(parse_parm_foward(d, fs, str) < 0){
+      log_message_katcp(d, KATCP_LEVEL_ERROR, NULL, "unable to parse %s into a parameter", str);
+      clear_forward_symbolic_state(fs);
+      return extra_response_katcp(d, KATCP_RESULT_FAIL, KATCP_FAIL_USAGE);
+    }
+  }
+
+  if(argc == 3){
+    if(load_parm_forward(d, fs, FOP_TRAIL, 0, NULL) < 0){
+      log_message_katcp(d, KATCP_LEVEL_ERROR, NULL, "unable to generate simple forwarding state for %s", cmd);
+      clear_forward_symbolic_state(fs);
+      return extra_response_katcp(d, KATCP_RESULT_FAIL, KATCP_FAIL_BUG);
+    }
+  }
+
+  /* this isn't quite ideal, we could end up handling a request handler within an inform map. Maybe eliminate the ptr code completely, not needed by add_full_cmd_map anyway */
+  ptr = default_message_type_katcm(cmd, KATCP_REQUEST);
+  if(ptr == NULL){
+    log_message_katcp(d, KATCP_LEVEL_ERROR, NULL, "unable to duplicate %s", cmd);
     clear_forward_symbolic_state(fs);
-    return extra_response_katcp(d, KATCP_RESULT_FAIL, KATCP_FAIL_BUG);
+    return extra_response_katcp(d, KATCP_RESULT_FAIL, KATCP_FAIL_MALLOC);
   }
 
   log_message_katcp(d, KATCP_LEVEL_TRACE, NULL, "about to register request %s to %s", ptr, fs->f_peer);
