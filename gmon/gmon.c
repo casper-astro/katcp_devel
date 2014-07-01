fc73a6da057a95f6b9513eeb41a97a3c7334be26
Rijan de Nysschen <rdenysschen@ska.ac.za>
2014-07-01 08:01:17 +0200

    Added fpga status check implementation.

--- gmon/gmon.c
+++ gmon/gmon.c
@@ -1,13 +1,30 @@
-#include "gmon.h"
 #include <stdio.h>
 #include <sys/select.h>
 #include <sys/time.h>
 #include <unistd.h>
 
+#include "gmon.h"
+
 #define BUFF_LEN    (1024)
 #define TIMEOUT_S   (5UL)
 
-int gmon_task(struct katcl_line *l);
+static int checkfpga(struct katcl_line *l)
+{
+    int retval = 0;
+
+    if (l == NULL) {
+        return -1;
+    }
+
+    retval += append_string_katcl(l, KATCP_FLAG_FIRST | KATCP_FLAG_STRING | KATCP_FLAG_LAST,
+            "?fpgastatus");
+
+    /* write katcl command out */
+    while ((retval = write_katcl(l)) == 0);
+
+    return retval;
+}
+
 
 int gmon_init(struct katcl_line *l)
 {
@@ -21,6 +38,10 @@ int gmon_task(struct katcl_line *l)
     fd_set readfds;
     struct timeval timeout;
     int retval;
+
+    if (l == NULL) {
+        return -1;
+    }
 
     fd = fileno_katcl(l);
     timeout.tv_sec = TIMEOUT_S;
@@ -52,6 +73,8 @@ int gmon_task(struct katcl_line *l)
     } else {
         printf("timeout after %ld seconds.\n", TIMEOUT_S);
     }
+
+    retval = checkfpga(l);
 
     return retval;
 }
