efb0a394cc0389cea50f1261b49c3af11c841197
Aaron Parsons <aparsons@berkeley.edu>
2022-03-14 22:57:40 +0000

    Fixed to work on RP4

--- tcpborphserver3/rpjtag_io.h
+++ tcpborphserver3/rpjtag_io.h
@@ -11,7 +11,7 @@
 #define PAGE_SIZE (4*1024)
 #define BLOCK_SIZE (4*1024)
 
-#define BCM2708_PERI_BASE        0x3F000000 // or for RPI 1 B+ 0x20000000
+#define BCM2708_PERI_BASE        0xFE000000 // RPI 2-3 0x3F000000 // or for RPI 1 B+ 0x20000000
 #define GPIO_BASE                (BCM2708_PERI_BASE + 0x200000) /* GPIO controller */
 
 // GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
@@ -35,7 +35,7 @@
 #ifdef DEBUG
 #define WAIT 10000000 //aprox 0.5s
 #else
-#define WAIT 10000 //aprox 0.5us
+#define WAIT 1000 //aprox 0.5us
 #endif
 
 int setup_io();
